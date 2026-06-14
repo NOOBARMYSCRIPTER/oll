f0600afe6b8aff87dac3e17b1e03e84e7c6b0441
Okho <109078241+NOOBARMYSCRIPTER@users.noreply.github.com>
2026-06-15 04:30:53 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,6 +19,21 @@ uint32_t original_br_x11 = 0xD61F0160;
 void* (*orig_sub_8C894)(void* result, unsigned int* data_block);
 unsigned int fake_block[16];
 
+uintptr_t get_module_base(const char* module_name) {
+    FILE* maps = fopen("/proc/self/maps", "r");
+    if (!maps) return 0;
+    char line[512];
+    uintptr_t base = 0;
+    while (fgets(line, sizeof(line), maps)) {
+        if (strstr(line, module_name) != nullptr) {
+            base = strtoull(line, nullptr, 16);
+            break;
+        }
+    }
+    fclose(maps);
+    return base;
+}
+
 void* my_sub_8C894(void* result, unsigned int* data_block) {
     uintptr_t current_block_addr = (uintptr_t)data_block;
     uintptr_t target_patched_addr = libstub_base + 0x5FB88;
@@ -26,9 +41,9 @@ void* my_sub_8C894(void* result, unsigned int* data_block) {
     if (current_block_addr <= target_patched_addr && target_patched_addr < (current_block_addr + 64)) {
         memcpy(fake_block, data_block, 64);
         size_t offset_in_block = (target_patched_addr - current_block_addr) / sizeof(unsigned int);
-        
+
         fake_block[offset_in_block] = original_br_x11;
-        
+
         __android_log_print(ANDROID_LOG_WARN, LOG_TAG, "🛡️ [CRC BYPASS] Скрыли патч на 0x5FB88 от SHA-256 сканера");
         
         return orig_sub_8C894(result, fake_block);
@@ -39,7 +54,7 @@ void* my_sub_8C894(void* result, unsigned int* data_block) {
 
 void loc_5FB88_instrument_callback(void* address, DobbyRegisterContext* ctx) {
     uintptr_t* raw_regs = (uintptr_t*)ctx;
-
+    
     uintptr_t x11_target = raw_regs[11];
     
     if (libstub_base == 0) {
@@ -58,21 +73,6 @@ void loc_5FB88_instrument_callback(void* address, DobbyRegisterContext* ctx) {
             __android_log_print(ANDROID_LOG_ERROR, "🏆 UNPACK_SUCCESS", "🎯 ПРЫЖОК В КУЧУ/ДИНАМИЧЕСКИЙ КОД: 0x%lx", x11_target);
         }
     }
-}
-
-uintptr_t get_module_base(const char* module_name) {
-    FILE* maps = fopen("/proc/self/maps", "r");
-    if (!maps) return 0;
-    char line[512];
-    uintptr_t base = 0;
-    while (fgets(line, sizeof(line), maps)) {
-        if (strstr(line, module_name) != nullptr) {
-            base = strtoull(line, nullptr, 16);
-            break;
-        }
-    }
-    fclose(maps);
-    return base;
 }
 
 void* interception_thread(void* arg) {
