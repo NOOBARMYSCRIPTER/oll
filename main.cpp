327447d86bbee8bf6cf73ec7b9dc691c87ee8433
Okho <109078241+NOOBARMYSCRIPTER@users.noreply.github.com>
2026-06-15 02:34:44 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <android/log.h>
 #include <string.h>
 #include <unwind.h>
+#include <thread>
+#include <chrono>
 #include <dlfcn.h>
 #include "dobby.h"
 
@@ -84,19 +86,21 @@ int my_log_buf_write(int bufID, int priority, const char* tag, const char* msg) 
 __attribute__((constructor)) void init() {
     LOGI("[+] NATIVE BYPASS LAUNCHED VIA DT_NEEDED!");
 
-    void* fork_addr = DobbySymbolResolver("libc.so", "fork");
-    if (fork_addr) {
-        DobbyHook(fork_addr, (void*)my_fork, (void**)&orig_fork);
-        LOGI("[+] Native hook on fork() successfully installed.");
-    } else {
-        LOGI("[-] Failed to find fork in libc.so");
-    }
-
-    void* log_addr = DobbySymbolResolver("liblog.so", "__android_log_buf_write");
-    if (log_addr) {
-        DobbyHook(log_addr, (void*)my_log_buf_write, (void**)&orig_log_buf_write);
-        LOGI("[+] Native hook on __android_log_buf_write successfully installed.");
-    } else {
-        LOGI("[-] Failed to find __android_log_buf_write in liblog.so");
-    }
+    std::thread([]() {
+        void* fork_addr = DobbySymbolResolver("libc.so", "fork");
+        if (fork_addr) {
+            DobbyHook(fork_addr, (void*)my_fork, (void**)&orig_fork);
+            LOGI("[+] Native hook on fork() successfully installed.");
+        } else {
+            LOGI("[-] Failed to find fork in libc.so");
+        }
+    
+        void* log_addr = DobbySymbolResolver("liblog.so", "__android_log_buf_write");
+        if (log_addr) {
+            DobbyHook(log_addr, (void*)my_log_buf_write, (void**)&orig_log_buf_write);
+            LOGI("[+] Native hook on __android_log_buf_write successfully installed.");
+        } else {
+            LOGI("[-] Failed to find __android_log_buf_write in liblog.so");
+        }
+    }).detach();
 }
